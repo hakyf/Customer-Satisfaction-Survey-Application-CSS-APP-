c5c09af9dbd0444b51058b9b4dcfcba797da9d3b
Habibullah Dzaky Musthafa <habibullahdm1@gmail.com>
2023-07-02 14:44:42 +0700

    add model clientapp

---
+++ clientapp/src/main/java/id/co/mii/clientapp/model/Result.java
@@ -0,0 +1,19 @@
+package id.co.mii.clientapp.model;
+
+import java.time.LocalDate;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Result {
+
+    private Long id;
+    private String score;
+    private LocalDate date;
+    private String mean;
+
+}
