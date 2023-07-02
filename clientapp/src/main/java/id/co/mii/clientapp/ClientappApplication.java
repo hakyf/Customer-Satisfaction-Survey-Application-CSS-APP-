25bf2f7d25b889d54402ba8b66f1e03fe2c34e05
Habibullah Dzaky Musthafa <habibullahdm1@gmail.com>
2023-07-02 17:31:35 +0700

    minor

--- clientapp/src/main/java/id/co/mii/clientapp/ClientappApplication.java
+++ clientapp/src/main/java/id/co/mii/clientapp/ClientappApplication.java
@@ -8,6 +8,10 @@ public class ClientappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ClientappApplication.class, args);
+
+		System.out.println("----------------------------------");
+		System.out.println("Client-App is Running . . .");
+		System.out.println("----------------------------------");
 	}
 
 }
