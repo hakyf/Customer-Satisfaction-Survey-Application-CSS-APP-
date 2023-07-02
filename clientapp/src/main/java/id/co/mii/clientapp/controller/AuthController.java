79a30f9b9535bdc6785f5794ae41d20bf6759dc3
Habibullah Dzaky Musthafa <habibullahdm1@gmail.com>
2023-07-02 17:03:12 +0700

    add controller & service login

---
+++ clientapp/src/main/java/id/co/mii/clientapp/controller/AuthController.java
@@ -0,0 +1,31 @@
+package id.co.mii.clientapp.controller;
+
+import id.co.mii.clientapp.model.dto.request.LoginRequest;
+import id.co.mii.clientapp.service.LoginService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/login")
+@AllArgsConstructor
+public class AuthController {
+
+    private LoginService loginService;
+
+    @GetMapping
+    public String loginPage(LoginRequest loginRequest) {
+        return "auth/login";
+    }
+
+    @PostMapping
+    public String login(LoginRequest loginRequest) {
+        if (!loginService.login(loginRequest)) {
+            return "redirect:/login?error=true";
+        }
+        return "redirect:/region";
+    }
+
+}
