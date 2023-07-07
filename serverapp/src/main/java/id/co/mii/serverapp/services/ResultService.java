5e1d077bc52ce5bb07c77f294a3fcfc1f0ad2528
Habibullah Dzaky Musthafa <habibullahdm1@gmail.com>
2023-07-07 10:41:27 +0700

    add service & controller result serverapp

---
+++ serverapp/src/main/java/id/co/mii/serverapp/services/ResultService.java
@@ -0,0 +1,44 @@
+package id.co.mii.serverapp.services;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import id.co.mii.serverapp.models.Result;
+import id.co.mii.serverapp.repository.ResultRepository;
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
+public class ResultService {
+    private ResultRepository resultRepository;
+
+    public List<Result> getAll() {
+        return resultRepository.findAll();
+    }
+
+    public Result getById(Long id) {
+        return resultRepository
+                .findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Status not found !"));
+    }
+
+    public Result create(Result result) {
+        return resultRepository.save(result);
+    }
+
+    public Result update(long id, Result result) {
+        getById(id);
+        result.setId(id);
+        return resultRepository.save(result);
+    }
+
+    public Result delete(long id) {
+        Result result = getById(id);
+        resultRepository.delete(result);
+        return result;
+    }
+
+}
